a94f0e779f0515f411aae9ff9740a5a6226e8b64
Nikita <hrynts2002@gmail.com>
2024-02-08 01:19:19 +0300

    Added country drop-down list

--- src/main/java/by/hryntsaliou/thymeleafdemo/model/Student.java
+++ src/main/java/by/hryntsaliou/thymeleafdemo/model/Student.java
@@ -6,13 +6,10 @@ public class Student {
 
     private String lastName;
 
+    private String country;
+
     public Student() {
 
-    }
-
-    public Student(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
     }
 
     public String getFirstName() {
@@ -29,5 +26,13 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 }
